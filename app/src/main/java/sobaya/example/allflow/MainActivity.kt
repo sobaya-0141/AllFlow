5453c45c82a1bb4e308afb81b87e40bd65942804
sobaya-0141 <soba.ha.kenkou@gmail.com>
2020-06-27 00:01:43 +0900

    add ActivityResultContracts

--- app/src/main/java/sobaya/example/allflow/MainActivity.kt
+++ app/src/main/java/sobaya/example/allflow/MainActivity.kt
@@ -1,7 +1,11 @@
 package sobaya.example.allflow
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import sobaya.example.allflow.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +18,23 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            test()
+        } else {
+            println("もう権限もってる")
+        }
+    }
+
+    fun test() {
+        val rp = ActivityResultContracts.RequestPermission()
+        val getContent = registerForActivityResult(rp) {
+            if (it) {
+                println("権限取得")
+            } else {
+                println("だめだった")
+            }
+        }
+        getContent.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
