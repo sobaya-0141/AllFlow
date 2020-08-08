7e3d8f19efdb12b7ec0ee6b2bfa84c428b970f86
sobaya-0141 <soba.ha.kenkou@gmail.com>
2020-08-08 21:45:22 +0900

    add in app review

--- app/src/main/java/sobaya/example/allflow/ui/main/MainFragment.kt
+++ app/src/main/java/sobaya/example/allflow/ui/main/MainFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.review.testing.FakeReviewManager
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.ldralighieri.corbind.view.clicks
 import ru.ldralighieri.corbind.widget.textChanges
 import sobaya.example.allflow.R
 
@@ -36,5 +41,26 @@ class MainFragment : Fragment() {
                 if (it.isEmpty()) return@onEach
                 viewModel.getRepo(it.toString())
             }.launchIn(viewLifecycleOwner.lifecycleScope)
+
+        view.findViewById<Button>(R.id.btnReview)
+            .clicks()
+            .onEach {
+//                val manager = ReviewManagerFactory.create(requireContext())
+                val manager = FakeReviewManager(requireContext())
+                val request = manager.requestReviewFlow()
+                request.addOnCompleteListener { request ->
+                    if (request.isSuccessful) {
+                        val reviewInfo = request.result
+                        val flow = manager.launchReviewFlow(requireActivity(), reviewInfo)
+                        flow.addOnCompleteListener { _ ->
+                            Toast.makeText(requireContext(), "終了", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        // 問題が発生したけど気にしたらいけない
+                    }
+                }
+
+            }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
     }
 }
