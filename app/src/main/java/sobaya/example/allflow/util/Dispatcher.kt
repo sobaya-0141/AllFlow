869686b82f436ba9efa9219e6a58cc71ed323269
shimoju <soba.ha.kenkou@gmail.com>
2021-01-29 13:09:31 +0900

    replace shared flow

--- app/src/main/java/sobaya/example/allflow/util/Dispatcher.kt
+++ app/src/main/java/sobaya/example/allflow/util/Dispatcher.kt
@@ -1,11 +1,10 @@
 package sobaya.example.allflow.util
 
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 
 class Dispatcher {
-    private val testProcessor = BroadcastChannel<String>(Channel.BUFFERED)
-    val testFlow = testProcessor.asFlow()
-    suspend fun testSend(args: String) = testProcessor.send(args)
+private val _testFlow = MutableSharedFlow<String>(1)
+    val testFlow: SharedFlow<String> = _testFlow
+    suspend fun testSend(args: String) = _testFlow.emit(args)
 }
