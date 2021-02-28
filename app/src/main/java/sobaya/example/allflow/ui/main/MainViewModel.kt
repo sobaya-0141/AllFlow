a1936699b528fda009f7c14c0e26cbe7cf7a5daf
shimoju <soba.ha.kenkou@gmail.com>
2021-03-01 02:31:00 +0900

    add catch

--- app/src/main/java/sobaya/example/allflow/ui/main/MainViewModel.kt
+++ app/src/main/java/sobaya/example/allflow/ui/main/MainViewModel.kt
@@ -4,10 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import sobaya.example.allflow.repository.GithubRepository
 
 class MainViewModel(private val repository: GithubRepository) : ViewModel() {
+
+    private val repo = repository.getRepo("sobaya-0141")
+
+    init {
+        repo.onEach {
+            it.body()
+        }.launchIn(viewModelScope)
+    }
 
     fun getRepo(user: String) {
         viewModelScope.launch {
