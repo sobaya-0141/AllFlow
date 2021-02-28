c34bce3b9cb89a125ac7fd07528bbf50c14a98c6
shimoju <soba.ha.kenkou@gmail.com>
2021-03-01 02:16:16 +0900

    add catch

--- app/src/main/java/sobaya/example/allflow/repository/GithubRepository.kt
+++ app/src/main/java/sobaya/example/allflow/repository/GithubRepository.kt
@@ -1,9 +1,12 @@
 package sobaya.example.allflow.repository
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class GithubRepository(private val service: GithubService) {
     fun getRepo(user: String) = flow {
         emit(service.listRepos(user))
-    }
+    }.catch { print(it.message) }.flowOn(Dispatchers.IO)
 }
